6288a2fed241ad998919e357a5f55112a1c135fd
Dave Vasilevsky <dave@vasilevsky.ca>
2016-09-08 16:41:00 -0400

    Handle null issue number

--- lib/timelog_report_patch.rb
+++ lib/timelog_report_patch.rb
@@ -1,8 +1,15 @@
 # Prints issues as links in time reports
 module TimelogHelperPatch
   def format_criteria_value(criteria_options, value)
-    return link_to_issue(Issue.find(value)) \
-      if criteria_options[:label] == :label_issue
+    # If it's an issue, and value looks like an integer, make it a link
+    if criteria_options[:label] == :label_issue
+      begin
+        intvalue = Integer(value)
+        return link_to_issue(Issue.find(value))
+      rescue
+      end
+    end
+
     super
   end
 end
