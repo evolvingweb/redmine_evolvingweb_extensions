a8dc7b0dd9e4e2b03534c94e0d4a62182b0ac2ea
Muhammad Inam Ur Rasheed <mohdinamshah@gmail.com>
2018-04-17 21:21:55 +0530

    Fix Redmine description field; refs 15599

---
+++ lib/editor_style_patch.rb
@@ -0,0 +1,15 @@
+# Fix the broken Issue description text area due to wiki plugin.
+
+require_dependency 'application_helper'
+
+class EditorStyleCSS < Redmine::Hook::ViewListener
+  def view_layouts_base_html_head(context={})
+    return unless ["WikiController", "IssuesController"].include?(context[:controller].class.name)
+
+    css_files = %w{editor_style_patch.css}
+
+    css_files.map do |file|
+      stylesheet_link_tag(file, :plugin=> 'redmine_evolvingweb_extensions')
+    end
+  end
+end
