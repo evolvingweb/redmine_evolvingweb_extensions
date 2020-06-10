69318449a7f0196998f4687410c8cfc984ee4872
Kevin Porras <kporras07@gmail.com>
2020-06-10 12:48:41 -0600

    refs #21476: Fix error on csv export.

--- lib/timelog_report_patch.rb
+++ lib/timelog_report_patch.rb
@@ -1,6 +1,6 @@
 # Prints issues as links in time reports
 module TimelogHelperPatch
-  def format_criteria_value(criteria_options, value)
+  def format_criteria_value(criteria_options, value, html=true)
     # If it's an issue, and value looks like an integer, make it a link
     if criteria_options[:label] == :label_issue
       begin
