cf57b693d774bcea60be45c0ccd3983198fa71ac
Kevin Porras <kporras07@gmail.com>
2020-06-22 07:13:14 -0600

    refs #21183: FFix regex.

--- assets/javascripts/image-notes.js
+++ assets/javascripts/image-notes.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $('.tab-content .has-details .wiki img, .description .wiki img').each(function() {
             if ($(this)[0].naturalWidth > 500) {
                 $(this).attr('width', 500);
-                const regex = /^\/attachments\/download\/(\d)\//;
+                const regex = /^\/attachments\/download\/(\d+)\//;
                 var matches;
                 var href;
                 if ((matches = regex.exec($(this).attr('src'))) !== null) {
