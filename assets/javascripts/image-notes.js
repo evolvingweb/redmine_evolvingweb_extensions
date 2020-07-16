de5686535a85653a127f4c99284ec876a212e4f4
Kevin Porras <kporras07@gmail.com>
2020-07-16 10:12:25 -0600

    refs #21183: Resize images in preview description and notes.

--- assets/javascripts/image-notes.js
+++ assets/javascripts/image-notes.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
-    setTimeout(function() {
-        $('.tab-content .has-details .wiki img, .description .wiki img').each(function() {
-            if ($(this)[0].naturalWidth > 500) {
-                $(this).attr('width', 500);
-                const regex = /^\/attachments\/download\/(\d+)\//;
-                var matches;
-                var href;
-                if ((matches = regex.exec($(this).attr('src'))) !== null) {
-                    if (matches[1]) {
-                        href = '/attachments/' + matches[1];
-                    }
-                }
-                if (href) {
-                    $(this).wrap('<a href="' + href + '"></a>');
+    function processImage($element) {
+        if ($element[0].naturalWidth > 500) {
+            $element.attr('width', 500);
+            const regex = /^\/attachments\/download\/(\d+)\//;
+            var matches;
+            var href;
+            if ((matches = regex.exec($element.attr('src'))) !== null) {
+                if (matches[1]) {
+                    href = '/attachments/' + matches[1];
                 }
             }
+            if (href) {
+                $element.wrap('<a href="' + href + '"></a>');
+            }
+        }
+    }
+    setTimeout(function() {
+        $('.tab-content .has-details .wiki img, .description .wiki img').each(function() {
+            processImage($(this));
         });
     }, 500);
+    $('.jstTabs > ul > li:nth-of-type(2)').click(function() {
+        console.log('Clicked preview tab');
+        setTimeout(function() {
+            $('.wiki-preview img').each(function() {
+                processImage($(this));
+            })
+        }, 500);
+    });
 });
