60f900f9b16bcd3747b2a988757ea3fcde43e1af
Kevin Porras <kporras07@gmail.com>
2021-03-18 07:56:37 -0600

    refs #25935: Add get_parents function to recursively search for all parents.

--- lib/query_model_patch.rb
+++ lib/query_model_patch.rb
@@ -1,8 +1,25 @@
 
 module QueryModelPatch
+  def get_parents(value, all = [])
+    if all.empty?
+      all = value
+    end
+    projects = Project.where(parent_id: value)
+    new_ids = []
+    projects.each do |project|
+      new_ids.push(project.id)
+      all.push(project.id)
+    end
+    unless new_ids.empty?
+      get_parents(new_ids, all)
+    else
+      all
+    end
+  end
   def sql_for_field(field, operator, value, db_table, db_field, is_custom_filter=false)
     if field == 'project_id' && operator == '=*'
-        value_str = value.join(",")
+        ids = get_parents(value)
+        value_str = ids.join(",")
         sql = "#{db_table}.#{db_field} IN (SELECT id FROM projects where id IN (#{value_str}) OR parent_id IN (#{value_str}))"
     else
         super
